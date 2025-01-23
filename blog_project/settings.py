9d54f4911c3249e49e44e3c2cc1a56dbffb7764a
Shohrux0812 <turobovshohrux992@gmail.com>
2025-01-23 15:35:53 +0500

    Initial commit

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0jroo%tagc#sl1(=5m4-p8rj6^@(gr*_791fhfy%y02qwvjhjh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
